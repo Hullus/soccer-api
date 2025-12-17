f73d1328aa2efc41b9789ccb2bfef6a3e95abb81
Hullus <73313334+Hullus@users.noreply.github.com>
2025-12-17 16:13:29 +0400

    Feat: added router.go

--- internal/http/router.go
+++ internal/http/router.go
@@ -1,8 +1,27 @@
 package http
 
-import "github.com/go-chi/chi/v5"
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
-func router() {
+func CreateRouter() *chi.Mux {
 	r := chi.NewRouter()
+	r.Use(middleware.AllowContentType("application/json"))
+	r.Use(middleware.CleanPath)
 
+	r.Group(func(r chi.Router) {
+		r.Get("/v1/me/team", nil)
+		r.Patch("/v1/me/team", nil)
+		r.Patch("/v1/me/players/{playerId}", nil)
+	})
+
+	r.Group(func(r chi.Router) {
+		r.Post("/v1/me/players/{playerId}/list", nil)
+		r.Delete("/v1/me/players/{playerId}/list\n", nil)
+		r.Get("/v1/market", nil)
+		r.Post("/v1/market/{listingId}/buy", nil)
+	})
+
+	return r
 }
--- main.go
+++ main.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+	internalHttp "soccer-api/internal/http"
+)
+
 func main() {
+	r := internalHttp.CreateRouter()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
